64eb7a1542eddb110e871c96647515e7917ef6b5
Nattajohn Rojanasupya <n.rojanasupya@gmail.com>
2020-03-11 14:48:58 -0700

    fixes viewContent returning a map object

--- src/room.py
+++ src/room.py
@@ -15,5 +15,42 @@ class Room:
         self.items.append(item)
 
     def viewContents(self):
-        print(f'Contents: {", ".join(map(lambda x: x.name, self.items))}')
+        print(f'Contents: {list(map(lambda x: x.name, self.items))}')
         print()
+
+
+'''
+# PYTHON
+class Item:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+torch = Item('torch', 'light the way')
+bag = Item('bag', 'holds things')
+
+items = [torch, bag]
+
+def getItemName(item):
+    return f'{item.name}: {item.description}'
+
+print(map(getItemName, items))
+
+# JS
+class Item {
+    constructor(name, description) {
+        this.name = name;
+        this.description = description;
+    }
+}
+
+const item1 = Item('torch', 'light the way')
+const item2 = Item('bag', 'holds things')
+
+console.log(item1.name)
+
+const items = [torch, bag];
+
+console.log(items.map(thingy => thingy.name));
+
+'''
