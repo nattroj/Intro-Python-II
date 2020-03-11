745b888edc5ad7a4efa419e24ac9ac01b34c2aa6
Nattajohn Rojanasupya <n.rojanasupya@gmail.com>
2020-03-11 12:04:52 -0700

    fixes array in memory bug

--- src/room.py
+++ src/room.py
@@ -2,14 +2,17 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description, items=[]):
+    def __init__(self, name, description, items=None):
         self.name = name
         self.description = description
-        self.items = items 
+        self.items = items if items else []
         self.n_to = None
         self.e_to = None
         self.s_to = None
         self.w_to = None
+    
+    def addItem(self, item):
+        self.items.append(item)
 
     def viewContents(self):
         print(f'Contents: {", ".join(map(lambda x: x.name, self.items))}')
